406b1dd030755f106b40938a5ed748b083683318
sunxiaod168 <sunxiaodong@sxd-mac.local>
2014-02-18 18:59:17 +0800

    HelloXcode
    new project

---
+++ HelloXcode/HelloXcode/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  HelloXcode
+//
+//  Created by 孙晓冬 on 14-2-18.
+//  Copyright (c) 2014年 孙晓冬. All rights reserved.
+//  test test
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+
+
+
+
+@end
