2cf511d500ee6109bdca44fca2bf7bbe707feaca
sunxiaod168 <sunxiaod168@hotmail.com>
2014-02-23 17:05:32 +0800

    GettingAttention is done!

--- GettingAttention/GettingAttention/ViewController.h
+++ GettingAttention/GettingAttention/ViewController.h
@@ -7,7 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIAlertViewDelegate, UIActionSheetDelegate>
+@property (strong, nonatomic) IBOutlet UILabel *userOutput;
+- (IBAction)doAlert:(id)sender;
+- (IBAction)doMultiButtonAlert:(id)sender;
+- (IBAction)doAlertInput:(id)sender;
+- (IBAction)doActionSheet:(id)sender;
+- (IBAction)doSound:(id)sender;
+- (IBAction)doAlertSound:(id)sender;
+- (IBAction)doVibration:(id)sender;
 
 @end
--- GettingAttention/GettingAttention/ViewController.m
+++ GettingAttention/GettingAttention/ViewController.m
@@ -26,4 +26,67 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)doAlert:(id)sender {
+    UIAlertView *alertDialog;
+    alertDialog = [[UIAlertView alloc] initWithTitle:@"Alert Button Selected" message:@"I need your attention now!" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil , nil];
+    [alertDialog show];
+}
+
+- (IBAction)doMultiButtonAlert:(id)sender {
+    UIAlertView *alertDialog;
+    alertDialog = [[UIAlertView alloc] initWithTitle:@"Alert Button selected" message:@"I need attention NOW!" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:@"Maybe Later",@"Never", nil];
+    [alertDialog show];
+}
+
+- (IBAction)doAlertInput:(id)sender {
+    UIAlertView *alertDialog;
+    alertDialog = [[UIAlertView alloc] initWithTitle:@"Email Address" message:@"Please enter your email address:" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil , nil];
+    alertDialog.alertViewStyle = UIAlertViewStylePlainTextInput;
+    [alertDialog show];
+
+}
+
+- (IBAction)doActionSheet:(id)sender {
+    UIActionSheet *actionSheet;
+    actionSheet = [[UIActionSheet alloc] initWithTitle:@"Available Actions" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:@"Destroy" otherButtonTitles:@"Negotiate",@"Compromise", nil];
+    actionSheet.actionSheetStyle = UIActionSheetStyleBlackTranslucent;
+    [actionSheet showFromRect:[(UIButton *) sender frame] inView:self.view animated:YES];
+    
+}
+
+- (IBAction)doSound:(id)sender {
+    SystemSoundID soundID;
+    NSString *soundFile = [[NSBundle mainBundle] pathForResource:@"soundeffect" ofType:@"wav"];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:soundFile], &soundID);
+    AudioServicesPlaySystemSound(soundID);
+}
+
+- (IBAction)doAlertSound:(id)sender {
+    SystemSoundID soundID;
+    NSString *soundFile = [[NSBundle mainBundle] pathForResource:@"alertsound" ofType:@"wav"];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:soundFile], &soundID);
+    AudioServicesPlayAlertSound(soundID);
+}
+
+- (IBAction)doVibration:(id)sender {
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+}
+- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    NSString *buttonTitle = [alertView buttonTitleAtIndex:buttonIndex];
+    if ([buttonTitle isEqualToString:@"Maybe Later"]) {
+        self.userOutput.text = @"Clicked 'Maybe Later'";
+    }else if([buttonTitle isEqualToString:@"Never"]){
+        self.userOutput.text = @"Clicked 'Never'";
+    }else{
+        self.userOutput.text = @"Clicked 'Ok'";
+    }
+    if ([alertView.title isEqualToString:@"Email Address"]) {
+        self.userOutput.text = [[alertView textFieldAtIndex:0] text];
+    }
+    
+}
+- (void) actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    NSString *buttonTitle = [actionSheet buttonTitleAtIndex:buttonIndex];
+    self.userOutput.text = [@"Clicked '" stringByAppendingFormat: @"%@%@",buttonTitle, @"'"];
+}
 @end
