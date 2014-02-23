66aad0d59871f85800dcb651e97b1879d10bf472
sunxiaod168 <sunxiaod168@hotmail.com>
2014-02-23 12:18:56 +0800

    New Project  GettingAttention

---
+++ GettingAttention/GettingAttention/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GettingAttention
+//
+//  Created by 孙晓冬 on 14-2-23.
+//  Copyright (c) 2014年 孙晓冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GettingAttention/GettingAttention/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GettingAttention
+//
+//  Created by 孙晓冬 on 14-2-23.
+//  Copyright (c) 2014年 孙晓冬. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
